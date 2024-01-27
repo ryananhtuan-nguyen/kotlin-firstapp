6fbd5a4a162330625da14e84eee0b88eb750997f
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-27 18:30:48 +1300

    added service for multiple posts

--- app/src/main/java/com/example/newapplication/service/PostService.kt
+++ app/src/main/java/com/example/newapplication/service/PostService.kt
@@ -7,4 +7,7 @@ import retrofit2.http.GET
 interface PostService {
     @GET("posts/1")
     suspend fun getPost(): PostResponse
+
+    @GET("posts")
+    suspend fun getPosts(): List<PostResponse>
 }
