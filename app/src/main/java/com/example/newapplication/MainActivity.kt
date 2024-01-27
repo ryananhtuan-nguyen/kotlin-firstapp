3fb1e0d0255c8a7629fe0209bb07a67c62d69ec0
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-27 18:29:28 +1300

    changed modifier

--- app/src/main/java/com/example/newapplication/MainActivity.kt
+++ app/src/main/java/com/example/newapplication/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
 
         setContent{
             ComposeMVVMTheme{
-                Surface(modifier = Modifier.fillMaxSize(), color=MaterialTheme.colors.background) {
+                Surface(color=MaterialTheme.colors.background) {
                     PostScreen(viewModel)
                 }
             }
